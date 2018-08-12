e03ed58fd2156964853b8429b74f6c9c20cbe5ad
JohnnyNich <39187279+JohnnyNich@users.noreply.github.com>
2018-08-12 22:32:51 +0800

    Trying out custom dots for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar example &
+polybar main -r &
+polybar bottom -r
 
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
-feh --bg-scale ~/.config/wall.png
-
-echo "Bars launched..."
+# polybar -c /home/void/.config/polybar/config top
